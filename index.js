8a8b52e2b7caef331d768b8239e4cfa3cc0c5a26
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-26 13:33:25 +0200

    Added start for the lambda service

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * 
+ * 
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  26 Jul. 2015
+ */
+ 
+/**
+ * Main entrypoint of the service.
+ * 
+ * @param {object}  event       The data regarding the event.
+ * @param {object}  context     The AWS Lambda execution context.
+ */
+exports.handler = function(event, context) {
+    context.succeed();
+};
