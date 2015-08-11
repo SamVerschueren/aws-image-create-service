996ec8b4fa066fe989d287926e789cc186b7a29b
Sam Verschueren <sam.verschueren@gmail.com>
2015-08-11 20:24:19 +0200

    Removed the random integer for a test

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ exports.handler = function(event, context) {
         
         // Build up the key
         var key = {
-            id: date.format('YYYY-MM-DD') + '_' + getRandomInt(1, 20),
+            id: date.format('YYYY-MM-DD'),
             date: date.format()
         };
         
@@ -62,11 +62,3 @@ exports.handler = function(event, context) {
         context.fail(err);
     });
 };
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
- */
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
